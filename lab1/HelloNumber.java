6275d93de53a9b29635996287451c28473fb8de2
weiliu <weiliu@outlook.com>
2020-07-29 21:09:04 -0400

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,12 @@
+
+public class HelloNumber {
+	public static void main(String[] args) {
+		int x = 0;
+		int a = 0;
+		while (x < 10) {
+			a = a + x;
+			System.out.println(a);
+			x = x +1;
+		}
+	}
+}
